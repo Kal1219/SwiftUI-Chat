48ddea6e666eec9244021f8bab390d4b862141b6
Kal1219 <kaleb.cortes1208@gmail.com>
2023-05-18 13:02:28 -0600

    Initial Commit

---
+++ SwiftUI Chat/SwiftUI_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChatApp.swift
+//  SwiftUI Chat
+//
+//  Created by Kaleb on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
